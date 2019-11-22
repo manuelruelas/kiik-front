e4fbf4f235c1cd8d01859787a6d2fab9e7c8bd81
manuelruelas <manuel.r.a@hotmail.com>
2019-11-21 21:53:59 -0700

    primeng modules exported

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CardModule} from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import {DropdownModule} from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
 
 const angular = [
   CommonModule
 ];
 const primeng = [
-  CardModule
+  CardModule,
+  InputTextModule,
+  DropdownModule,
+  ButtonModule
 ];
 
 const modules = [
