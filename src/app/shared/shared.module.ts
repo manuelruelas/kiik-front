91ebd5d16cfda7f7636c0e14fea28ed767603d06
Manuel Ruelas <manuel.r.a@hotmail.com>
2019-11-21 17:09:13 -0700

    Shared module created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CardModule} from 'primeng/card';
 
+const angular = [
+  CommonModule
+];
+const primeng = [
+  CardModule
+];
 
+const modules = [
+  ...angular,
+  ...primeng
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    ...modules
+  ],
+  exports: [
+    ...modules
   ]
 })
 export class SharedModule { }
