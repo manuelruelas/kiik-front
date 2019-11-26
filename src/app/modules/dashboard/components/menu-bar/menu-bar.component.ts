fd5c0d3aada0e8aa54c1f1114feb2637f87f842a
Manuel Ruelas <manuel.r.a@hotmail.com>
2019-11-26 15:09:44 -0700

    Prime ng removed

--- src/app/modules/dashboard/components/menu-bar/menu-bar.component.ts
+++ src/app/modules/dashboard/components/menu-bar/menu-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,24 +6,9 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent implements OnInit {
-  items: MenuItem[];
   constructor() { }
 
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Voluntarios',
-        icon: 'pi pi-users'
-      },
-      {
-        label: 'Peticiones',
-        icon: 'pi pi-globe'
-      },
-      {
-        label: 'Perfil',
-        icon: 'pi pi-user'
-      }
-    ];
   }
 
 }
