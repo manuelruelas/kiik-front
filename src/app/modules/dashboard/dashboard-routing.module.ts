5b9566373482e2cd4fd1368075ee9852d89f695a
manuelruelas <manuel.r.a@hotmail.com>
2019-12-02 23:10:57 -0700

    Volunteer and request added

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { VolunteersComponent  } from "./pages/volunteers/volunteers.component";
+import { RequestComponent } from './pages/request/request.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, children: [
     {path:'', component:HomeComponent},
+    {path:'volunteers', component: VolunteersComponent },
+    {path:'request', component:RequestComponent}
   ]}
 ];
 @NgModule({
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,6 +6,12 @@ import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { VolunteersComponent } from './pages/volunteers/volunteers.component';
+import { VolunteerCardComponent } from './components/volunteer-card/volunteer-card.component';
+import { RequestComponent } from './pages/request/request.component';
+import { RequestCardComponent } from './components/request-card/request-card.component';
 
 const components = [
   MenuBarComponent
@@ -18,13 +24,23 @@ const pages = [
 @NgModule({
   declarations: [
     ...components,
-    ...pages
+    ...pages,
+    LoginComponent,
+    SignupComponent,
+    VolunteersComponent,
+    VolunteerCardComponent,
+    RequestComponent,
+    RequestCardComponent
     
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     SharedModule
+  ],
+  entryComponents:[
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class DashboardModule { }
