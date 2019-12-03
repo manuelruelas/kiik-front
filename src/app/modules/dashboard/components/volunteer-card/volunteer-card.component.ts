dbd9d705bcbac77aeccfecd04234c35017a3d3bf
manuelruelas <manuel.r.a@hotmail.com>
2019-12-02 23:12:07 -0700

    Volunter card created

---
+++ src/app/modules/dashboard/components/volunteer-card/volunteer-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-card',
+  templateUrl: './volunteer-card.component.html',
+  styleUrls: ['./volunteer-card.component.css']
+})
+export class VolunteerCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
