5a0cfe21754cb8a2f41ee3f0e6982ae82be0093e
manuelruelas <manuel.r.a@hotmail.com>
2019-12-02 23:13:05 -0700

    home show modal

--- src/app/modules/dashboard/pages/home/home.component.ts
+++ src/app/modules/dashboard/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog,DialogPosition } from '@angular/material/dialog';
+import { LoginComponent } from '../../components/login/login.component';
+import { SignupComponent } from '../../components/signup/signup.component';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  images;
-  constructor() { }
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
-   
+
   }
 
+  showLoginModal() {
+    
+    let dialogRef = this.dialog.open(LoginComponent,{
+      width:"500px"
+    });
+
+    dialogRef.afterClosed();
+
+  }
+
+  showSignupModal(){
+    let dialogRef = this.dialog.open(SignupComponent,{
+      width:"500px"
+    });
+    
+  }
 }
